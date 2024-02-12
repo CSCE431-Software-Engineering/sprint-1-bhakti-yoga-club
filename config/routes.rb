91686aa07392946f71666c860308372467800689
Matthew S McDonough <matthewsmcd2002@tamu.edu>
2024-02-11 19:34:04 -0600

    Fixed some routing issues and fixed rpsec request tests for member

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,14 @@ Rails.application.routes.draw do
   get 'announcements/:id', to: 'announcements#show', as: 'announcement'
   post 'announcements', to: 'announcements#create'
 
-  get 'members', to: 'members#index'
+  get 'members', to: 'members#index', as: 'members'
   get 'members/new', to: 'members#new', as: 'new_member'
-  post 'members', to: 'members#create'
+  post 'members', to: 'members#create', as: 'create_member'
   get 'members/:id/edit', to: 'members#edit', as: 'edit_member'
   patch 'members/:id', to: 'members#update', as: 'update_member'
   get 'members/:id/delete', to: 'members#delete', as: 'confirm_delete_member'
   delete 'members/:id', to: 'members#destroy', as: 'delete_member'
+  get 'members/:id', to: 'members#show', as: 'member'
 
   
   root "pages#home"
