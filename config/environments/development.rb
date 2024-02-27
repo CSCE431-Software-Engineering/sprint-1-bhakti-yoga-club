005755401483224cb93f95413307e5059d04743a
Steven7zzz <q1824711081@gmail.com>
2024-02-27 15:45:44 -0600

    add heroku url

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
 
-  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'byc-dev-announcements-t-z0solp.herokuapp.com' }
 
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
