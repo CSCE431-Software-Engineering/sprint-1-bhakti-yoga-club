3807559cb0801892b3ed2f83f3f0c994a10b3e03
MatthewMcD02-AM <matthewsmcd2002@tamu.edu>
2024-02-28 14:10:41 -0600

    Added migration to add 'is_on_mailing_list' column to members. Default value is 'false'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_07_032038) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_28_200713) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -70,6 +70,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_07_032038) do
     t.date "date_left"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_on_mailing_list", default: false
     t.index ["email"], name: "index_members_on_email", unique: true
   end
 
